4aba5a6a0c9e5059cc896a99f8a9f144de2010b0
Yash Lamba <yashlamba2000@gmail.com>
2021-08-23 02:00:28 +0530

    ui: nav: made nav stick to the top (#14)

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="flexbox-container">
-            <div><a href="#home">‎<button>Home</button></a></div>
-            <div><a href="#about"><button>‏‏‎About‎‏‏‎‎</button></a></div>
-            <div><a href="#howtouse"><button>How To Use?</button></a></div>
-            <div><a href="https://cod-ed.github.io/handwrite" target="_blank"><button>Documentation</button></a></div>
+        <div className="navbar-background">
+            <div className="flexbox-container">
+                <div>
+                    <a href="#home">
+                        ‎<button className="home">Home</button>
+                    </a>
+                </div>
+                <div>
+                    <a href="#about">
+                        <button>‏‏‎About‎‏‏‎‎</button>
+                    </a>
+                </div>
+                <div>
+                    <a href="#howtouse">
+                        <button>How To Use?</button>
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href="https://cod-ed.github.io/handwrite"
+                        target="_blank"
+                    >
+                        <button>Documentation</button>
+                    </a>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Navbar;
