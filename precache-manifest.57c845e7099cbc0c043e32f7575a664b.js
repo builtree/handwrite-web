096db5121ad61b2c3412cf8029e6f078330aada1
yashlamba <yashlamba2000@gmail.com>
2021-09-07 11:33:53 +0000

    Updates

---
+++ precache-manifest.57c845e7099cbc0c043e32f7575a664b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f67dc84785174344f977",
+    "url": "https://cod-ed.github.io/handwrite-web/static/css/main.fe4f7003.chunk.css"
+  },
+  {
+    "revision": "f67dc84785174344f977",
+    "url": "https://cod-ed.github.io/handwrite-web/static/js/main.0ae50021.chunk.js"
+  },
+  {
+    "revision": "529f8ff2a44b45e374c4",
+    "url": "https://cod-ed.github.io/handwrite-web/static/js/runtime~main.57609e05.js"
+  },
+  {
+    "revision": "1d5cd4166ec2e01c3f9a",
+    "url": "https://cod-ed.github.io/handwrite-web/static/css/2.4a676243.chunk.css"
+  },
+  {
+    "revision": "1d5cd4166ec2e01c3f9a",
+    "url": "https://cod-ed.github.io/handwrite-web/static/js/2.34f2686d.chunk.js"
+  },
+  {
+    "revision": "54b0d79c10e8c4d1ebefe9c44a44eab3",
+    "url": "https://cod-ed.github.io/handwrite-web/static/media/logo.54b0d79c.svg"
+  },
+  {
+    "revision": "9dacc4d7d5064db94dd5b8659556de79",
+    "url": "https://cod-ed.github.io/handwrite-web/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://cod-ed.github.io/handwrite-web/precache-manifest.57c845e7099cbc0c043e32f7575a664b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://cod-ed.github.io/handwrite-web/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
