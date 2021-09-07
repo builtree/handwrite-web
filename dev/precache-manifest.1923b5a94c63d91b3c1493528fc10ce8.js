0c7394266247337063acd96ec8569f5be3dc041f
GitHub Action <github-action@github.com>
2021-09-07 12:22:23 +0000

    Updates

---
+++ dev/precache-manifest.1923b5a94c63d91b3c1493528fc10ce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f67dc84785174344f977",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/static/css/main.fe4f7003.chunk.css"
+  },
+  {
+    "revision": "f67dc84785174344f977",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/static/js/main.0ae50021.chunk.js"
+  },
+  {
+    "revision": "a9fe5b86d8825f861d65",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/static/js/runtime~main.bb78cd9b.js"
+  },
+  {
+    "revision": "1d5cd4166ec2e01c3f9a",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/static/css/2.4a676243.chunk.css"
+  },
+  {
+    "revision": "1d5cd4166ec2e01c3f9a",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/static/js/2.34f2686d.chunk.js"
+  },
+  {
+    "revision": "54b0d79c10e8c4d1ebefe9c44a44eab3",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/static/media/logo.54b0d79c.svg"
+  },
+  {
+    "revision": "abfe5a81e7074d027dcfa23295a29bf6",
+    "url": "https://cod-ed.github.io/handwrite-web/dev/index.html"
+  }
+];
